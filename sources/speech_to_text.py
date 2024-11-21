ba1d632ad35d983b4522ce9f54a1a8e8a5fb53d9
calvin.paumier <calvin.paumier-ext@iledefrance-mobilites.fr>
2024-11-21 14:29:40 +0100

    add text to speech

---
+++ sources/speech_to_text.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import speech_recognition as sr
+
+
+def recognize_speech() -> str:
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            st.info("Enregistrement en cours")
+            audio = recognizer.listen(source, timeout=15)
+            st.success("Traitement en cours")
+            text = recognizer.recognize_google(audio, language="fr-FR")
+            return text
+        except sr.UnknownValueError:
+            return "Je n'ai pas compris, veuillez réessayer."
+        except sr.RequestError as e:
+            return f"Erreur du service : {e}"
