214a32bdb94fd3057d91ca2f2f5ba94024e6721e
Théophile Molcard <molcard.t@gmail.com>
2024-11-21 14:40:12 +0100

    FEAT: Add riti from adress (#5)

--- test_api.py
+++ test_api.py
@@ -1,39 +1,54 @@
 import os
+import urllib.parse
 
 import requests
 import pandas as pd
 
-API_BASE_URL = 'https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/journeys?from='
 PRIM_API_TOKEN = os.environ['PRIM_API_TOKEN']
+HEADERS = {'Accept': 'application/json', 'apikey': PRIM_API_TOKEN}
+
+API_BASE_URL_PLACES = 'https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/places?'
+API_BASE_URL_JOURNEY = 'https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/journeys?'
 
 
-def call_api(dlong: str, dlat: str, along: str, alat: str, jour: str):
-    # URL de l'API
-    destination = dlong + "%3B%20" + dlat + "&to=" + along + "%3B%20" + alat + "&datetime=" + jour
-    url = API_BASE_URL + destination
-
-    # Le header doit contenir la clé API : apikey, remplacer #VOTRE CLE API par votre clé API
-    headers = {'Accept': 'application/json', 'apikey': PRIM_API_TOKEN}
+def get_place(adresse: str) -> str:
+    params = {'q': adresse}
+    url = API_BASE_URL_PLACES + urllib.parse.urlencode(params)
 
     # Envoi de la requête au serveur
-    req = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
 
-    # Affichage du code réponse
-    print('Status:', req)
+    if response.status_code != 200:
+        raise ValueError(f"Error code {response.status_code}: {response.text}")
+    response_dict = response.json()
+    return response_dict["places"][0]["id"]
 
-    # Lecture du json
-    return pd.json_normalize(req.json())
+
+def call_riti(origin: str, destination: str, jour: str):
+    # URL de l'API
+    params = {
+        'from': get_place(origin),
+        'to': get_place(destination),
+        'datetime': jour,
+    }
+
+    url = API_BASE_URL_JOURNEY + urllib.parse.urlencode(params)
+
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code != 200:
+        raise ValueError(f"Error code {response.status_code}: {response.text}")
+    return pd.json_normalize(response.json())
 
 
 if __name__ == "__main__":
-    # longitude / latitude
-    dlong = "2.33792"
-    dlat = "48.85827"
-    along = "2.3588523"
-    alat = "48.9271087"
+    # origin / destination
+    origin = "14 rue du Prévôt, 75004 Paris"
+    destination = "Censier-Daubenton"
 
     # Date et heure du trajet
     jour = "20241121T073000"
 
-    response = call_api(dlong=dlong, dlat=dlat, along=along, alat=alat, jour=jour)
+    response = call_riti(origin=origin, destination=destination, jour=jour)
+
     print(response)
